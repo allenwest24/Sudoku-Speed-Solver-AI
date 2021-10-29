420c08a8d92e57f9b280f6787258b7f4374e1f65
Allen West <58156836+allenwest24@users.noreply.github.com>
2021-10-29 19:00:34 -0400

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -1,25 +1,33 @@
 class Solution:
+    # TODO: Implement.
+    # Modifies the current list of valid numbers to only include valid numbers for the Row.
     def allowedInRow(possibleNumbers, board, ii, jj):
         return possibleNumbers
     
+    # TODO: Implement.
+    # Modifies the current list of valid numbers to only include valid numbers for the Column.
     def allowedInColumn(possibleNumbers, board, ii, jj):
         return possibleNumbers
-    
+
+    # TODO: Implement.
+    # Modifies the current list of valid numbers to only include valid numbers for the box.
     def allowedInBox(possibleNumbers, board, ii, jj):
         return possibleNumbers
     
+    # Sees if there is only one possible solution, making it a definite placement.
     def checkIfOnlySolution(board, ii, jj):
         possibleNumbers = '123456789'
+        
+        # TODO: Add more heuristics to narrow down the options.
         possibleNumbers = allowedInRow(possibleNumbers, board, ii, jj)
         possibleNumbers = allowedInColumn(possibleNumbers, board, ii, jj)
         possibleNumbers = allowedInBox(possibleNumbers, board, ii, jj)
         if len(possibleNumbers) == 1:
             return True, possibleNumbers[0]
     
+    # Solves the puzzle in-place.
     def solveSudoku(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
+        # Goes through by a Depth-First Search.
         for ii in range(len(board)):
             for jj in range(len(board[ii])):
                 curr = board[ii][jj]
