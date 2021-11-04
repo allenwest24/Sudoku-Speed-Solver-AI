13bf83f2ea4a190df323331e9619211f7677010a
Allen West <58156836+allenwest24@users.noreply.github.com>
2021-11-04 19:23:58 -0400

    Update naiveDFSSolution.go

--- naiveDFSSolution.go
+++ naiveDFSSolution.go
@@ -1,6 +1,17 @@
-// This is the naive implementation of backtracking.
-
 const SQUARE_LENGTH = 9
+
+func isSolved(board [][]byte) bool {
+    var solved = true
+    for ii := 0; ii < SQUARE_LENGTH; ii++ {
+        for jj := 0; jj < SQUARE_LENGTH; jj++ {
+            if board[ii][jj] == '.' {
+                solved = false
+                break
+            }
+        }
+    }
+    return solved
+}
 
 func isValid(board [][]byte, row, column int, val byte) bool {
 	for ii := 0; ii < SQUARE_LENGTH; ii++ {
@@ -22,25 +33,26 @@ func isValid(board [][]byte, row, column int, val byte) bool {
 	return true
 }
 
-func solveSudoku(board [][]byte) {
-	// Depth first search.
-    for ii := 0; ii < SQUARE_LENGTH; ii++ {
-        for jj := 0; jj < SQUARE_LENGTH; jj++ {
-            var allowedValues = []byte
-            if (board[ii][jj] != '.') {
-				continue
-			}
-			for val := byte('1'); val <= byte('9'); val++ {
-                // Check to see if this value fits into the current solution.
-				if isValid(board, ii, jj, val) {
-                    allowedValues = append(allowedValues, val)
-			}
-            if len(allowedValues) == 1 {
-                board[ii][jj] = allowedValues[0]
-            } else {
-                board[ii][jj] = '.'
+func solveSudoku(board [][]byte)  {
+    for !isSolved(board) {
+        for ii := 0; ii < SQUARE_LENGTH; ii++ {
+            for jj := 0; jj < SQUARE_LENGTH; jj++ {
+                var allowedValues []byte
+                if (board[ii][jj] != '.') {
+                    continue
+                }
+                for val := byte('1'); val <= byte('9'); val++ {
+                    // Check to see if this value fits into the current solution.
+                    if isValid(board, ii, jj, val) {
+                        allowedValues = append(allowedValues, val)
+                    }
+                }
+                 if len(allowedValues) == 1 {
+                    board[ii][jj] = allowedValues[0]
+                } else {
+                    board[ii][jj] = '.'
+                }
             }
-		}
-	}
+        }
+    }
 }
-
