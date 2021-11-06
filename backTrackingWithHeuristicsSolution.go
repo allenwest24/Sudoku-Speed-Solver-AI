86ec4196e9729d576af7328e7358cf77a4435239
Allen West <58156836+allenwest24@users.noreply.github.com>
2021-11-06 18:46:48 -0400

    Works, but is now much slower. Will fix.

--- backTrackingWithHeuristicsSolution.go
+++ backTrackingWithHeuristicsSolution.go
@@ -2,6 +2,19 @@
 
 const SQUARE_LENGTH = 9
 
+func isSolved(board [][]byte) bool {
+    var solved = true
+    for ii := 0; ii < SQUARE_LENGTH; ii++ {
+        for jj := 0; jj < SQUARE_LENGTH; jj++ {
+            if board[ii][jj] == '.' {
+                solved = false
+                break
+            }
+        }
+    }
+    return solved
+}
+
 func isValid(board [][]byte, row, column int, val byte) bool {
 	for ii := 0; ii < SQUARE_LENGTH; ii++ {
         // Is this value already in this column?
@@ -23,15 +36,13 @@ func isValid(board [][]byte, row, column int, val byte) bool {
 	return true
 }
 
-func assessBoard(board [][]byte) [][]int {
-    var heuristicBoard [][]int
+func assessBoard(board [][]byte) [9][9]int {
+    var heuristicBoard [9][9]int
     var valuesAllowedInCurr int = 0
-    var tempRow []int
     for ii := 0; ii < SQUARE_LENGTH; ii++ {
         for jj := 0; jj < SQUARE_LENGTH; jj++ {
             if (board[ii][jj] != '.') {
-                tempRow = append(tempRow, valuesAllowedInCurr)
-                valuesAllowedInCurr = 0
+                heuristicBoard[ii][jj] = 0
 				continue
 			}
             for val := byte('1'); val <= byte('9'); val++ {
@@ -40,48 +51,53 @@ func assessBoard(board [][]byte) [][]int {
 					valuesAllowedInCurr++
                 }
             }
-            tempRow = append(tempRow, valuesAllowedInCurr)
+            heuristicBoard[ii][jj] = valuesAllowedInCurr
             valuesAllowedInCurr = 0
         }
-        heuristicBoard = append(heuristicBoard, tempRow)
-        tempRow = tempRow[:0]
     }
     return heuristicBoard
 }
 
 // Solve the sudoku puzzle.
 func solve(board [][]byte) bool {
-    var heuristicBoard [][]int = assessBoard(board)
+    var heuristicBoard [9][9]int = assessBoard(board)
     fmt.Print(heuristicBoard)
+    var currBest = 0
+    var currBestii = 0
+    var currBestjj = 0
     // Depth first search.
     for ii := 0; ii < SQUARE_LENGTH; ii++ {
         for jj := 0; jj < SQUARE_LENGTH; jj++ {
-            // If this square has already been solved, move down the branch.
-            if (board[ii][jj] != '.') {
-				continue
-			}
+            if ((currBest == 0 || heuristicBoard[ii][jj] < currBest) && heuristicBoard[ii][jj] != 0) {
+                currBest = heuristicBoard[ii][jj]
+                currBestii = ii
+                currBestjj = jj
+            }
+        }
+    }
+    if currBest == 0 && !isSolved(board) {
+        return false
+    }
+    if currBest == 0 {
+        return true
+    }
             
-            // For every valid value that fits within this box, call solve on it.
-            // This will resemble a decision tree.
-			for val := byte('1'); val <= byte('9'); val++ {
-                // Check to see if this value fits into the current solution.
-				if isValid(board, ii, jj, val) {
-					board[ii][jj] = val
-					if solve(board) {
-						return true
-					} else {
-                        // We leave the '.' in this box so we can see where it went wrong.
-						board[ii][jj] = '.'
-					}
-                } 
+    // For every valid value that fits within this box, call solve on it.
+    // This will resemble a decision tree.
+	for val := byte('1'); val <= byte('9'); val++ {
+        // Check to see if this value fits into the current solution.
+		if isValid(board, currBestii, currBestjj, val) {
+			board[currBestii][currBestjj] = val
+			if solve(board) {
+				return true
+			} else {
+                // We leave the '.' in this box so we can see where it went wrong.
+				board[currBestii][currBestjj] = '.'
 			}
-            // No values fit. Break this branch.
-			return false
-		}
+        } 
 	}
-    // If we got through the entire board, we have successfully solved this puzzle.
-    // Backtrack to the root of the tree and say so.
-	return true
+    // No values fit. Break this branch.
+	return false
 }
 
 // Invokes a helper method that can recursively call itself then return the valid solution.
