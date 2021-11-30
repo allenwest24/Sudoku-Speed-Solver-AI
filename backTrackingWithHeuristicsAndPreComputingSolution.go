784b82410198c2b714662b00bbfd32da336f794d
Allen West <58156836+allenwest24@users.noreply.github.com>
2021-11-29 20:49:10 -0500

    Add files via upload

--- backTrackingWithHeuristicsAndPreComputingSolution.go
+++ backTrackingWithHeuristicsAndPreComputingSolution.go
@@ -1,4 +1,12 @@
+package main
+
+import (
+        "fmt"
+	"time"
+)
+
 const SQUARE_LENGTH = 9
+const ROOT = 3
 
 func isSolved(board [][]byte) bool {
     var solved = true
@@ -22,10 +30,10 @@ func isValid(board [][]byte, row, column int, val byte) bool {
         // Is this value already in this row?
         if board[row][ii] == val {
 			return false
-		}
+		} 
         // Is this value already in this box?
-        boxRow := ((row / 3) * 3) + (ii / 3)
-        boxColumn := ((column / 3) * 3) + (ii % 3)
+        boxRow := ((row / ROOT) * ROOT) + (ii / ROOT)
+        boxColumn := ((column / ROOT) * ROOT) + (ii % ROOT)
 		if board[boxRow][boxColumn] == val {
 			return false
 		}
@@ -39,7 +47,8 @@ func solve(board [][]byte, heuristicQueue []int) bool {
     var currii int
     var currjj int
     if len(heuristicQueue) == 0 {
-        return true
+	solved := isSolved(board)
+        return solved
     } else {
         currii = heuristicQueue[0] / 10
         currjj = heuristicQueue[0] % 10
@@ -61,9 +70,22 @@ func solve(board [][]byte, heuristicQueue []int) bool {
 }
 
 // Invokes a helper method that can recursively call itself then return the valid solution.
-func solveSudoku(board [][]byte) {
-    //var heuristicQueue []int = prioritize(board)
+func main() {
+    board := [][]byte {
+            {byte('.'),byte('4'),byte('.'),byte('.'),byte('.'),byte('2'),byte('.'),byte('.'),byte('.')},
+            {byte('.'),byte('.'),byte('8'),byte('4'),byte('7'),byte('.'),byte('.'),byte('.'),byte('5')},
+            {byte('.'),byte('.'),byte('.'),byte('.'),byte('.'),byte('6'),byte('.'),byte('7'),byte('.')},
+            {byte('.'),byte('6'),byte('.'),byte('9'),byte('1'),byte('.'),byte('.'),byte('5'),byte('.')},
+            {byte('3'),byte('.'),byte('.'),byte('.'),byte('.'),byte('.'),byte('.'),byte('.'),byte('8')},
+            {byte('.'),byte('.'),byte('.'),byte('.'),byte('.'),byte('7'),byte('.'),byte('.'),byte('.')},
+            {byte('.'),byte('9'),byte('.'),byte('5'),byte('4'),byte('.'),byte('.'),byte('1'),byte('.')},
+            {byte('.'),byte('.'),byte('6'),byte('.'),byte('.'),byte('.'),byte('9'),byte('.'),byte('.')},
+            {byte('.'),byte('.'),byte('.'),byte('2'),byte('.'),byte('.'),byte('.'),byte('.'),byte('.')}}
     var heuristicQueue = []int{43, 74, 65, 45, 75, 62, 60, 53, 44, 35, 32, 23, 15, 11, 3, 88, 87, 85, 84, 78, 77, 73, 68, 51, 47, 41, 38, 36, 30, 24, 21, 20, 17, 16, 10, 8, 7, 6, 4, 82, 81, 80, 66, 57, 56, 54, 52, 46, 28, 26, 22, 0, 86, 71, 70, 58, 50, 42, 2}
-
+    start := time.Now()
     solve(board, heuristicQueue)
+    t := time.Now()
+    elapsed := t.Sub(start)
+    fmt.Print(elapsed)
+    fmt.Print("\n\n")
 }
